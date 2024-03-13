b19308751e36e43ac1821b2bf8c2c495d56a72a0
Tasank <apanasovich04@mail.ru>
2024-03-13 15:20:53 +0500

    Добавление стиля в html И обновление репозитория

--- Обучение/training.py
+++ Обучение/training.py
@@ -1,5 +1,7 @@
-from random import randint
-a = int(input('Нижняя граница:'))
-b = int(input('Верхняя граница:'))
-n = randint(a, b)
-print(n)
+table = [[''] * 10 for i in range(1, 11)]
+print()
+print('    | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 |')
+for i, row in enumerate(table):
+    row_str = f"  {i} | {' | '.join(row)}"
+    print(row_str)
+    print('  -------------------------------------------')
