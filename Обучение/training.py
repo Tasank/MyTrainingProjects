59f9c18cceb1ad734789fc0b37ae3059ac821a90
Tasank <apanasovich04@mail.ru>
2024-03-17 17:37:01 +0500

    Обновление

--- Обучение/training.py
+++ Обучение/training.py
@@ -1,7 +1,18 @@
-table = [[''] * 10 for i in range(1, 11)]
-print()
-print('    | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 |')
-for i, row in enumerate(table):
-    row_str = f"  {i} | {' | '.join(row)}"
-    print(row_str)
-    print('  -------------------------------------------')
+from random import randint
+combo_1 = ''
+combo_2 = ''
+count = 0
+
+
+while combo_1 != 'PPP' and combo_2 != 'OOO':
+    a = randint(0, 1)
+    if a == 0:
+        combo_1 += 'P'
+        print('P', end=' ')
+    elif a == 1:
+        combo_2 += 'O'
+        print('O', end=' ')
+    count += 1
+
+print(combo_1)
+print(combo_2)
