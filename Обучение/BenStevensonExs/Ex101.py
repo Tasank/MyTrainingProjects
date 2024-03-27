488023820daf221f567cf9d9c85bc88b98cfc2d6
Tasank <apanasovich04@mail.ru>
2024-03-27 15:03:27 +0500

    Случайный номерной знак.

--- Обучение/BenStevensonExs/Ex101.py
+++ Обучение/BenStevensonExs/Ex101.py
@@ -4,3 +4,25 @@
 При этом номера в старом и новом форматах должны создаваться примерно с одинаковой вероятностью.
 В основной программе нужно сгенерировать и вывести на экран случайный номерной знак.
 """
+from random import randint
+def generate():
+    old_new = randint(3, 4)
+    mark = ''
+    # генерируем номерной знак старого типа
+    if old_new == 3:
+        for i in range(old_new):
+            mark += chr(randint(65, 90))
+        for i in range(old_new):
+            mark += str(randint(0, 9))
+        print('Ваш случайный номерной знак - старого типа: ', mark)
+    # генерируем номерной знак нового типа
+    elif old_new == 4:
+        for i in range(old_new):
+            mark += str(randint(0, 9))
+        for i in range(3):
+            mark += chr(randint(65, 90))
+        print('Ваш случайный номерной знак - нового типа: ', mark)
+
+def main():
+    generate()
+main()
