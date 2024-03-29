70b3048f34c14ab78b22421d42fb781f0651ae94
Tasank <apanasovich04@mail.ru>
2024-03-29 11:31:27 +0500

    Конвертация систем счисления

--- Обучение/BenStevensonExs/Ex105.py
+++ Обучение/BenStevensonExs/Ex105.py
@@ -7,3 +7,41 @@
 конвертирующую число из произвольной системы счисления в десятичную, и обратную функцию, переводящую значение из
 десятичной системы в произвольную.
 """
+
+def system(original_system, base_system, number):
+    if base_system > 16 or base_system < 2:
+        return 'Ошибка'
+
+    result = back_convert_10(number, original_system)
+    result = convert_10(result, base_system)
+    return result
+
+# Произвольная система счисления в десятичное число
+# Второй аргумент это основание системы счисления самого числа
+def back_convert_10(number, base):
+    result = int(number, base)
+    return result
+
+# Из десятичной системы исчисления в любую
+def convert_10(value, base):
+    digits = '0123456789abcdefghijklmopqrstuvwxyz'
+    if base > len(digits): return None
+    result = ''
+    while value > 0:
+        result = digits[value % base] + result
+        value //= base
+    return result
+
+def display():
+    print('________Конвертация систем счисления________')
+    print('           (2)   (8)   (10)   (16)          ')
+    print('Введите число интересующий системы счисления.')
+    print('_____________________________________________')
+    # Ввод
+    original = int(input('Введите изначальную систему счисления: '))
+    base = int(input('Введите новую систему счисления: '))
+    number = (input('Введите число для преобразования: '))
+    # Вывод
+    print('____________________________________')
+    print('Ваш результат:', system(original, base, number))
+display()
