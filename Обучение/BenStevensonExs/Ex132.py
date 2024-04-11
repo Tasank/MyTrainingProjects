3f7d79dc4effbee15586757a5b8c5b80e25cdb44
Tasank <apanasovich04@mail.ru>
2024-04-11 11:56:22 +0500

    Решение постф.выражений

--- Обучение/BenStevensonExs/Ex132.py
+++ Обучение/BenStevensonExs/Ex132.py
@@ -4,3 +4,26 @@
 постфиксную форму, выполняющую полученное выражение и выводящую на экран результат.
 Используйте при решении задачи свои наработки из упражнений 129, 130 и 131, а также алгоритм, приведенный выше(В книге)
 """
+from Ex131 import infixToPostfix
+
+def DecisionPostfix(postfix_tokens):
+    values = []
+    for tokens in postfix_tokens:
+        if tokens in '+-*/':
+            right = int(values.pop())
+            left = int(values.pop())
+            if tokens == '+': res = left + right
+            elif tokens == '-': res = left - right
+            elif tokens == '*': res = left * right
+            else: res = left // right
+            values.append(res)
+        else:
+            values.append(tokens)
+    return values[0]
+def main():
+    expressions = input('Введите математическое выражение: ')
+    # Постфиксный результат
+    postfix = infixToPostfix(expressions)
+    # вывод
+    print(DecisionPostfix(postfix))
+main()
