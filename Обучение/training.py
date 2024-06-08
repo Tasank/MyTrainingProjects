b2996f0a88e7e01b90ddb4cd015ee156dbfce8ba
Tasank <apanasovich04@mail.ru>
2024-06-08 20:05:30 +0500

    135

--- Обучение/training.py
+++ Обучение/training.py
@@ -1,18 +1,5 @@
-from random import randint
-combo_1 = ''
-combo_2 = ''
-count = 0
-
-
-while combo_1 != 'PPP' and combo_2 != 'OOO':
-    a = randint(0, 1)
-    if a == 0:
-        combo_1 += 'P'
-        print('P', end=' ')
-    elif a == 1:
-        combo_2 += 'O'
-        print('O', end=' ')
-    count += 1
-
-print(combo_1)
-print(combo_2)
+text = "1+1 - 2+ 1 * 2 /1 + (35-5)"
+t = text.split()
+b = []
+if len(b) == 0:
+    print('1')
