d78ce14f7ec6dfb71dd836079b8d823ff3064e78
Tasank <apanasovich04@mail.ru>
2024-06-08 16:58:26 +0500

    Решение 133

--- Обучение/BenStevensonExs/Ex133.py
+++ Обучение/BenStevensonExs/Ex133.py
@@ -5,3 +5,31 @@
 True только в том случае, если список smaller является подсписком списка larger.
 Напишите также основную программу для демонстрации работы функции.
 """
+larger = [1, 2, 3, 4, 5]
+smaller = [4, 5]
+
+
+
+def isSublist(larger, smaller):
+    if not smaller:
+        return True
+    elif smaller == larger:
+        return True
+    for i in range(len(larger) - len(smaller) + 1):
+        # Сравниваем срез с larger с подсписком smaller
+        if larger[i:i + len(smaller)] == smaller:
+            return True
+    return False
+
+
+def main():
+    larger = list(map(int, input('Введите список чисел через пробел: ').split()))
+    smaller = list(map(int, input('Введите подсписок чисел через пробел: ').split()))
+
+    if isSublist(larger, smaller):
+        print('Этот подсписок является частью списка.')
+    else:
+        print('Этот подсписок не является частью списка.')
+
+if __name__ == '__main__':
+    main()
