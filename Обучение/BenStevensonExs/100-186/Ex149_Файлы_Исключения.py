248fb3d72259a85c42499d0bf59f108d9795dcfa
Tasank <apanasovich04@mail.ru>
2024-06-21 19:11:43 +0500

    149

--- Обучение/BenStevensonExs/100-186/Ex149_Файлы_Исключения.py
+++ Обучение/BenStevensonExs/100-186/Ex149_Файлы_Исключения.py
@@ -5,3 +5,21 @@
 Напишите программу на Python, имитирующую поведение этой утилиты. Если файла, указанного пользователем,
 не существует, или не задан аргумент командной строки, необходимо вывести соответствующее сообщение об ошибке.
 """
+# content = 'exemple.txt'
+def read_ten_str_file(content):
+    try:
+        read_file = open(content, 'r')
+        for i in range(10):
+            line = read_file.readline()
+            if line:
+                print(line)
+            else:
+                break
+        read_file.close()
+
+    except FileNotFoundError:
+        print(f'Файл {content} не найден')
+
+content = input('Введите имя файла: ')
+
+read_ten_str_file(content)
