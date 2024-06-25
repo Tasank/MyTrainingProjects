10d655d8adb691e24864a8434f791e53d07ecfbd
Tasank <apanasovich04@mail.ru>
2024-06-25 20:43:31 +0500

    155 Решение

--- Обучение/BenStevensonExs/100-186/Ex155.py
+++ Обучение/BenStevensonExs/100-186/Ex155.py
@@ -6,27 +6,42 @@
 словам и исключив из них пробелы и знаки препинания. Также при подсчете частоты появления слов в
 файле вам стоит игнорировать регистры.
 """
-
-def most_frequent_words(filename):
-    word_counts = Counter()
-
-    with open(filename, 'r') as file:
-        for line in file:
-            # Приведение строки к нижнему регистру и удаление знаков препинания
-            cleaned_line = clean_text(line.lower())
-            words = cleaned_line.split()
-            word_counts.update(words)
-
-    # Нахождение максимальной частоты
-    max_frequency = max(word_counts.values(), default=0)
-
-    # Нахождение слов с максимальной частотой
-    most_frequent = [word for word, count in word_counts.items() if count == max_frequency]
-
-    return most_frequent, max_frequency
+# Импортируем необходимые функции из модуля Ex117
+from Ex117 import word
 
 
-filename = input('Введите имя файла для обработки: ')
-words, frequency = most_frequent_words(filename)
-print(f'Самое частое слово(а): {", ".join(words)} (встречается {frequency} раз)')
+def frequent_words(filename):
+    try:
+        # Открываем файл для чтения
+        with open(filename, 'r') as readfile:
+            word_count = {}  # Словарь для подсчета частоты слов
 
+            # Проходим по каждой строке в файле
+            for line in readfile:
+                # Приводим строку к нижнему регистру и разбиваем на слова
+                words = word(line.lower())
+
+                # Проходим по каждому слову в строке
+                for w in words:
+                    if w in word_count:
+                        word_count[w] += 1  # Увеличиваем счетчик, если слово уже встречалось
+                    else:
+                        word_count[w] = 1  # Инициализируем счетчик, если слово встречается впервые
+
+            # Находим максимальное значение частоты слов
+            max_count = max(word_count.values())
+
+            # Находим все слова, которые встречаются максимальное количество раз
+            most_frequent_words = [w for w, count in word_count.items() if count == max_count]
+
+            return most_frequent_words
+    except FileNotFoundError:
+        return 'Не удалось открыть файл.'
+
+
+# Запрашиваем у пользователя имя файла
+filename = input("Введите имя файла: ")
+# Получаем список самых частых слов
+most_frequent_words = frequent_words(filename)
+# Выводим результат
+print(f'Самое частое слово (слова): {most_frequent_words}')
