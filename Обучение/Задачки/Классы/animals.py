89b6e2f49d061fe37e8154cb9a19b7bbb9c506c7
Tasank <apanasovich04@mail.ru>
2024-08-17 21:19:43 +0700

    Классы практика

--- Обучение/Задачки/Классы/animals.py
+++ Обучение/Задачки/Классы/animals.py
@@ -1,4 +1,6 @@
 """Классы животных"""
+
+from random import choice
 # Определения класса
 class Dog:
     def __init__(self, age, name):
@@ -7,14 +9,59 @@ class Dog:
         self.name = name
     # Метод класса
     def hungry(self):
-        print(f'{self.name}: Уаф! РРррр! Грр! Уав! Уаф! Оуу? Ууууу( ')
+        print(f'|{self.name}: Уаф! РРррр! Грр! Уав! Уаф! Оуу? Ууууу( ')
 
 # Создание объектов класса
 one_dog = Dog(3, 'Boby')
 two_dog = Dog(2, 'Crazy')
 
+print('|_____________________')
+print('|   Проверка класса Dog.')
+
 # Доступ к атрибутам
-print(f' Возраст собаки: {two_dog.age}')
+print(f'|Возраст собаки: {two_dog.age}')
 
 # Вызов методов
 two_dog.hungry()
+
+print('|   Конец проверки    ')
+print('|_____________________')
+class Predator:
+    def __init__(self, age, name, habitat, gender):
+        self.age = age
+        self.name = name
+        self.habitat = habitat
+        self.gender = gender
+
+    def info(self):
+        print((f'Объект - {self.name}| возраст - {self.age}| место обитания - {self.habitat}| особь - ({self.gender})'
+                f' пола.'))
+
+
+# Множества для генерации случайных объектов
+age_set = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
+name_set = {'Кошка     ', 'Волк      ', 'Ястреб    ', 'Крокодил  ', 'Стервятник', 'Лев       ', 'Барс      ',
+            'Леопард   '}
+habitat_set = {'Московский зоопарк', 'Каньон', 'Тайга', 'Степь', 'Горы', 'Тундра', 'Тропики', 'Пустыня'}
+gender_set = {'мужского', "женского"}
+
+object_dict = {}
+
+ask_count = int(input('Введите желаемое количество создания объектов(число): '))
+for i in range(ask_count):
+    age = choice(list(age_set))
+    name = choice(list(name_set))
+    habitat = choice(list(habitat_set))
+    gender = choice(list(gender_set))
+
+
+    predator = Predator(age, name, habitat, gender)
+    predator.info()
+
+    object_dict[f'predator_{i}'] = predator
+
+print(f'Всего объектов: {len(object_dict)}')
+
+# Для проверки содержимого словаря
+for key, predator in object_dict.items():
+    print(f'{key}: {predator.age}, {predator.habitat}, {predator.gender}')
