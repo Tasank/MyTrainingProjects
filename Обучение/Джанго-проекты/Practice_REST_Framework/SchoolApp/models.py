984271d6b2b91a1f3768a8a669ca2487a0e1a20d
Tasank <apanasovich04@mail.ru>
2024-09-05 20:58:32 +0500

    Джанго проект

---
+++ Обучение/Джанго-проекты/Practice_REST_Framework/SchoolApp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class School(models.Model):
+   name = models.CharField(max_length=64, unique=True)
+   address = models.CharField(max_length=120)
+
+class SClass(models.Model):
+   grade = models.IntegerField()
+   school = models.ForeignKey(School, on_delete=models.CASCADE)
+
+
+class Student(models.Model):
+   name = models.CharField(max_length=64)
+   sclass = models.ForeignKey(SClass, on_delete=models.CASCADE)
+
