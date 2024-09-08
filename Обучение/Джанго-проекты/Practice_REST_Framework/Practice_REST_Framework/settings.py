5e54625ede203409143288528c4422aa5e6ce1dd
Tasank <apanasovich04@mail.ru>
2024-09-08 22:05:10 +0500

    REST

--- Обучение/Джанго-проекты/Practice_REST_Framework/Practice_REST_Framework/settings.py
+++ Обучение/Джанго-проекты/Practice_REST_Framework/Practice_REST_Framework/settings.py
@@ -123,3 +123,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+   'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+   'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+   'PAGE_SIZE': 10,
+   'DEFAULT_PERMISSION_CLASSES': [
+       'rest_framework.permissions.IsAuthenticated',
+   ]
+
+}
+
