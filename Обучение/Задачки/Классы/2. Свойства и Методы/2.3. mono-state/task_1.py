684a8a986b4c5180c539aa561913388b12750d74
Tasank <apanasovich04@mail.ru>
2024-09-09 16:19:03 +0500

    N1

---
+++ Обучение/Задачки/Классы/2. Свойства и Методы/2.3. mono-state/task_1.py
@@ -0,0 +1,17 @@
+class Robot:
+    robot_info_attr = {
+        'i1': '0000А1',
+        'i2': 'Уборщик',
+        'i3': '2 месяца',
+    }
+    def __init__(self):
+        self.__dict__ = Robot.robot_info_attr
+        print(self.__dict__)
+
+N1 = Robot()
+N2 = Robot()
+N3 = Robot()
+N3.i1 = '0000А2'
+N3.i2 = 'Садовник'
+N3.i3 = '6 месяцев'
+N3 = Robot()
