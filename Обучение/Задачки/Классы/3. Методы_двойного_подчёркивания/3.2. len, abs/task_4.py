317a8b4a5552d00c70ddb18dd8bc07f8470e2410
Tasank <apanasovich04@mail.ru>
2024-09-23 17:59:52 +0500

    T4

---
+++ Обучение/Задачки/Классы/3. Методы_двойного_подчёркивания/3.2. len, abs/task_4.py
@@ -0,0 +1,21 @@
+"""
+Класс "Словарь": Создайте класс MyDict, который хранит словарь ключ-значение и имеет метод __len__,
+который возвращает количество пар ключ-значение в словаре.
+"""
+class MyDict:
+    def __init__(self):
+        self.dict = {}
+
+    def add (self, key, value):
+        self.dict[key] = value
+
+    def __len__(self):
+        print(f"Количество пар ключ-значение в словаре: {len(self.dict)}")
+
+
+my_dict = MyDict()
+key = input('Введите ключ: ')
+value = input('Введите значение: ')
+my_dict.add(key, value)
+
+my_dict.__len__()
