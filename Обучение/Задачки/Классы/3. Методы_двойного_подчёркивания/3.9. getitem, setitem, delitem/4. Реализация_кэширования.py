beda2aba41c12114a00be91452fe74465332eb10
Tasank <apanasovich04@mail.ru>
2024-11-12 20:51:23 +0500

    N4

--- Обучение/Задачки/Классы/3. Методы_двойного_подчёркивания/3.9. getitem, setitem, delitem/4. Реализация_кэширования.py
+++ Обучение/Задачки/Классы/3. Методы_двойного_подчёркивания/3.9. getitem, setitem, delitem/4. Реализация_кэширования.py
@@ -3,3 +3,25 @@
 Реализуйте методы __getitem__ и __setitem__,
 чтобы данные хранились в кэше и при доступе к ним они не загружались заново.
 """
+
+class Cache():
+    def __init__(self):
+        self.cache = {}
+
+    def __getitem__(self, item):
+        return self.cache[item]
+
+    def __setitem__(self, key, value):
+        self.cache[key] = value
+
+    def __repr__(self):
+        return repr(self.cache)
+
+# Пример использования
+cache = Cache()
+
+cache["key"] = "value"
+print(cache["key"])  # выведет "value"
+
+cache["key"] = "new_value"
+print(cache["key"])  # выведет "new_value"
