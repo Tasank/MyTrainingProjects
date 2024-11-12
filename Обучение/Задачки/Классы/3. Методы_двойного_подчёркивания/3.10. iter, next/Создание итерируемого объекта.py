f68031e8a602b93e3c4138c9f85d03373ef5b4ea
Tasank <apanasovich04@mail.ru>
2024-11-12 21:38:53 +0500

    reliz

---
+++ Обучение/Задачки/Классы/3. Методы_двойного_подчёркивания/3.10. iter, next/Создание итерируемого объекта.py
@@ -0,0 +1,25 @@
+"""
+Создайте класс Countdown, который будет итерируемым и позволит вам выполнять обратный отсчет
+от заданного числа до 1 iterуйтnext __iter__ и __next__, чтобы сделать объект итерируемым.
+
+Подсказка: Обратный отсчет должен завершаться, когда достигнута 1, и вызывать StopIteration.
+
+"""
+import time
+class Countdown:
+    def __init__(self, start):
+        self.start = start
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.start <= 1:
+            raise StopIteration
+        else:
+            self.start -= 1
+            time.sleep(1)
+            return self.start
+
+for i in Countdown(6):
+    print(i)
