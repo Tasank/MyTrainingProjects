fda47eb5cfa294e3fb56d96a71af9cbd9bb73ac2
Tasank <apanasovich04@mail.ru>
2024-11-13 20:55:45 +0500

    add

---
+++ Обучение/Задачки/Классы/4. Исключение/4.1. Наследование_principle/Банковские счета.py
@@ -0,0 +1,3 @@
+"""
+Создайте класс BankAccount с методами deposit(), withdraw() и check_balance(). Затем создайте классы CheckingAccount и SavingsAccount, которые наследуют свойства и методы класса BankAccount. Добавьте специфичные методы для каждого класса, например, CheckingAccount может иметь метод write_check(), а SavingsAccount - add_interest().
+"""
