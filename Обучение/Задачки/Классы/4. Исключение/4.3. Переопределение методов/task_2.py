27743a3918cc75e3e729666c3e7c39599e627abb
Tasank <apanasovich04@mail.ru>
2024-11-15 17:48:27 +0500

    Решение 15.11.2024

--- Обучение/Задачки/Классы/4. Исключение/4.3. Переопределение методов/task_2.py
+++ Обучение/Задачки/Классы/4. Исключение/4.3. Переопределение методов/task_2.py
@@ -1,5 +1,22 @@
-"""
- Создайте новый класс Surgeon, который является подклассом Doctor. Переопределите метод sleep,
- чтобы он печатал "Surgeon нуждается в большем количестве сна".
- Создайте экземпляр Surgeon и вызовите метод combo.
-"""
+class Doctor:
+    def action(self):
+        print('Я умею лечить')
+
+    def _info(self):
+        print('Я - врач')
+
+
+    def play(self):
+        self.action()
+        self._info()
+
+class Surgeon(Doctor):
+    def action(self):
+        print('Я занимаюсь хирургией')
+
+    def _info(self):
+        print('Я - хирург')
+
+
+d = Surgeon()
+d.play()
