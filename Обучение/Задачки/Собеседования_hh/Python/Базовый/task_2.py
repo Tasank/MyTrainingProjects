6dd9e259814bb2ca9d5fdd20b8d6974fdde302d0
Tasank <apanasovich04@mail.ru>
2024-12-07 21:44:01 +0500

    add 07.12.2024

--- Обучение/Задачки/Собеседования_hh/Python/Базовый/task_2.py
+++ Обучение/Задачки/Собеседования_hh/Python/Базовый/task_2.py
@@ -9,7 +9,32 @@
 
 
 def palindrome(text):
+    # 1 вариант(с циклом)
     for i in range(len(text) - 1, -1, -1):
         print(text[i], end='')
+
+    print()
+
+    # 2 вариант (с удалением элемента из конца списка)
+    test_1 = list(text)
+    data =''
+    for _ in range(len(test_1)):
+        data += test_1.pop()
+    print(data)
+
+    # 3 вариант (с добавлением элемента в начало списка)
+    dat = []
+    for value in text:
+        dat.insert(0, value)
+    print(''.join(dat))
+
+    # 4 вариант(конкатенация)
+    output = ''
+    for value in text:
+        output = value + output
+    print(output)
+
+
+
 
 palindrome('Тестовый текст')
