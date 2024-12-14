ba3d08c139380340d8a2018fc13c217628fc4792
Tasank <apanasovich04@mail.ru>
2024-12-14 17:01:08 +0500

    task 14.12.2024

--- Обучение/Задачки/Собеседования_hh/Python/Средний/task_4.py
+++ Обучение/Задачки/Собеседования_hh/Python/Средний/task_4.py
@@ -3,11 +3,11 @@
 Ваша задача — проверить список паролей и определить, какие из них безопасны.
 Пароль считается таковым, если он удовлетворяет пяти условиям:
 
-Содержит хотя бы одну заглавную букву;
-Содержит хотя бы одну строчную букву;
-Содержит хотя бы одну цифру;
-Содержит хотя бы один специальный символ (допустимый набор символов: !@#$%^&*()-+);
-Длина пароля больше или равна 8 символам.
+1) Содержит хотя бы одну заглавную букву;
+2) Содержит хотя бы одну строчную букву;
+3) Содержит хотя бы одну цифру;
+4) Содержит хотя бы один специальный символ (допустимый набор символов: !@#$%^&*()-+);
+5) Длина пароля больше или равна 8 символам.
 
 Формат ввода
 Одна строка, в которой чередуются пароли, разделенные пробелами. Длина строки — не более 100 символов.
@@ -29,3 +29,31 @@ Password1 Pass@word 12345 pass!word
 Выходные данные:
 Не найдено
 """
+
+special_symbols = "!@#$%^&*()-+"
+
+
+class CheckPasswords:
+    def __init__(self, passwords):
+        self.passwords = passwords.split()
+
+    def is_safe_password(self, password):
+        if len(password) < 8:
+            return False
+        has_upper = any(char.isupper() for char in password)
+        has_lower = any(char.islower() for char in password)
+        has_digit = any(char.isdigit() for char in password)
+        has_special = any(char in special_symbols for char in password)
+        return has_upper and has_lower and has_digit and has_special
+
+    def solve(self):
+        safe_passwords = [password for password in self.passwords if self.is_safe_password(password)]
+        if safe_passwords:
+            return " ".join(safe_passwords)
+        else:
+            return "Не найдено"
+
+
+input_string = input()
+checker = CheckPasswords(input_string)
+print(checker.solve())
