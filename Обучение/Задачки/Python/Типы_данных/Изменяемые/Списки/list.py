557b0eb236dc6860655c98275e90da384f754fae
Tasank <apanasovich04@mail.ru>
2024-12-30 13:34:34 +0500

    add 30.12.2024

--- Обучение/Задачки/Python/Типы_данных/Изменяемые/Списки/list.py
+++ Обучение/Задачки/Python/Типы_данных/Изменяемые/Списки/list.py
@@ -1,3 +1,26 @@
 """
-Здесь должна быть задача по спискам
+Дан произвольный список. Представьте его в обратном порядке.
 """
+
+our_list = ['1', 2, 3]
+
+# 1 вариант (реверс)
+one_list = list(reversed(our_list))
+# our_list.reverse()
+print(one_list, end='\n\n')
+
+# 2 вариант (срез)
+two_list = our_list[::-1]
+print(two_list, end='\n\n')
+
+# 3 вариант (цикл)
+three_list = []
+for i in range(len(our_list)-1, -1, -1):
+    three_list.append(our_list[i])
+print(three_list, end='\n\n')
+
+# 4 вариант
+four_list = []
+for el in our_list:
+    four_list = [el] + four_list
+print(four_list, end='\n\n')
