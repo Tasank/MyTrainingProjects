a7d7e2ce4e1f747874e586aba188df902d4d45e2
Tasank <apanasovich04@mail.ru>
2025-01-11 23:41:23 +0500

    up 11.01.2025

--- Обучение/Обектное мышление/3_Человек.py
+++ Обучение/Обектное мышление/3_Человек.py
@@ -2,10 +2,17 @@ class Human:
     def __init__(self, name: str, age: int, gender: str, height: int, weight: int, health: str):
         self.name = name
         self.age = age
-        self.gender = gender
         self.height = height
         self.weight = weight
         self.health = health
+
+        gender_list = ['мужской', 'женский']
+        if gender.lower() in gender_list:
+            self.gender = gender
+        else:
+            self.gender = 'мужской'
+
+        self.povestka()
 
     def hello_me(self):
         print(f'Привет, меня зовут {self.name}')
@@ -14,6 +21,10 @@ class Human:
         print(f'Мой рост - {self.height} см')
         print(f'Мой вес - {self.weight} кг')
         print(f'Моя здоровье - [{self.health}]', end='\n\n')
+
+    def povestka(self):
+        if self.gender == 'мужской' and self.age >= 18:
+            print('Пришла повестка')
 
     def birthday(self):
         self.age += 1
@@ -27,7 +38,7 @@ class Human:
             print('Задача не выполнена.', e)
 
 
-Alya = Human('Алия', 20, 'девушка', 170, 60, 'здорова')
+Alya = Human('Алия', 20, 'женский', 170, 60, 'здорова')
 Alya.hello_me()
 
 
