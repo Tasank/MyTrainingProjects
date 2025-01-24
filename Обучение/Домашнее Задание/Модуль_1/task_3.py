48beb75d6b6d95351b2317cc7689ada7e104f337
Tasank <apanasovich04@mail.ru>
2025-01-24 22:28:06 +0500

    update 24.01.2025

---
+++ Обучение/Домашнее Задание/Модуль_1/task_3.py
@@ -0,0 +1,10 @@
+estimation = int(input('Введите оценку: '))
+
+if estimation == 5:
+    print('Отлично потрудился!')
+elif estimation == 4:
+    print('Ты молодец!')
+elif estimation == 3:
+    print('Не страшно ты всё равно молодец!')
+elif estimation == 2:
+    print('Тебе стоит потрудиться!')
