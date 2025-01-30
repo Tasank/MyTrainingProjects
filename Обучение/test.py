3f1e2f611a95d9d8c52c91bed8b06a6e6ea52547
Tasank <apanasovich04@mail.ru>
2025-01-30 21:31:22 +0500

    add 30.01.2025

---
+++ Обучение/test.py
@@ -0,0 +1,24 @@
+num = 5
+text = 'Строка'
+is_text = True
+pi = 3.14
+
+
+print(type(num))
+print(type(text))
+print(type(is_text))
+print(type(pi))
+
+
+
+
+
+
+
+
+
+
+# num_list = [1, 2, 3, 4, 5] # Список
+# num_dict = {'a': 1, 'b': 2, 'c': 3} # Словарь
+# num_set = {1, 2, 3, 4, 5} # Множество
+# num_tuple = (1, 2, 3, 4, 5) # Кортеж
