b291c21ad3c077254fc6e710e0266a735e50cc62
Tasank <apanasovich04@mail.ru>
2025-02-04 21:25:40 +0500

    up 04.02.2025

--- Обучение/Домашнее Задание/Модуль_1/task_5.py
+++ Обучение/Домашнее Задание/Модуль_1/task_5.py
@@ -1,21 +1,4 @@
-# %s - строка
-# %d - целое число
-# %f - дробное число
-
-
-
-
-
-
-text = 'World'
-print("Hello, %s!" % text)
-
-
-age = 15
-print("Мне %d лет" % age)
-
-pi = 3.1415926213123123123136346
-print("Pi = %.2f" % pi)
-
-name = 'Паша'
-print('|%20s|' % name)
+list_str = ['Первое слово', 'римская цифра', 'император', 'водосборник',
+            'еда была вкусной', 'теперь я зна всё']
+print(list_str[0][0], list_str[1][0], list_str[2][0], list_str[3][0], list_str[4][0], list_str[5][0])
+print(f'{list_str[0][0]}{list_str[1][0]}{list_str[2][0]}{list_str[3][0]}{list_str[4][0]}{list_str[5][0]}!')
