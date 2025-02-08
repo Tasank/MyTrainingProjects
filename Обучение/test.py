0a299040b5895fe704e705ce36112f574f9bf81d
Tasank <apanasovich04@mail.ru>
2025-02-08 20:53:04 +0500

    up 08.02.2025

--- Обучение/test.py
+++ Обучение/test.py
@@ -1,19 +1,5 @@
-num = 5
-text = 'Строка'
-is_text = True
-pi = 3.14
-
-
-print(type(num))
-print(type(text))
-print(type(is_text))
-print(type(pi))
-
-
-
-
-
-num_list = [1, 2, 3, 4, 5] # Список
-num_dict = {'a': 'name', 'b': 2, 'c': 3} # Словарь
-num_set = {1, 2, 3, 4, 5} # Множество
-num_tuple = (1, 2, 3, 4, 5) # Кортеж
+print("13")
+print("13")
+print("13")
+print("13")
+print("13")
