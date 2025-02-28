779967304c64ed877a57e07cc2133662bd7c5c07
Tasank <apanasovich04@mail.ru>
2025-02-28 19:32:48 +0500

    обновление 28.02.2025

---
+++ Обучение/Домашнее Задание/Модуль_1/task_10.py
@@ -0,0 +1,33 @@
+"""
+Напиши функцию, которая получает строку, разбивает её на слова и возвращает список из этих слов,
+записанных задом наперёд.
+"""
+
+text = 'Мышка украла сыр со стола'
+
+
+def reverse_text_1(text):
+    list_reverse = text.split()
+    return list_reverse[::-1]
+
+
+# Решение без использования split()
+def reverse_text_2(text):
+    words = []
+    word = ''
+
+    for char in text:
+        if char != ' ':
+            word += char
+        else:
+            if word:
+                words.append(word)
+                word = ''
+    # Добавляем последнее слова, если строка не заканчивается пробелом
+    if word:
+        words.append(word)
+    return words[::-1]
+
+
+print(reverse_text_1(text))
+print(reverse_text_2(text))
