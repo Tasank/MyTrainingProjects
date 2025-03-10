87be7fcab367f7ea2e5e47e6d34015fd6cbc037b
Tasank <apanasovich04@mail.ru>
2025-03-10 22:20:17 +0500

    обновление 10.03.2025

--- Обучение/Домашнее Задание/Модуль_1/task_12.py
+++ Обучение/Домашнее Задание/Модуль_1/task_12.py
@@ -1,40 +1,78 @@
 import turtle
 
-pen = turtle.Pen()
-pen.shape("turtle")
-pen.pensize(7)
+# Создаем экран
+screen = turtle.Screen()
+screen.bgcolor("white")
+
+# Создаем черепашек
+red = turtle.Turtle()
+blue = turtle.Turtle()
+yellow = turtle.Turtle()
+green = turtle.Turtle()
+
+# Меняем им цвет
+red.color("red")
+blue.color("dark blue")
+yellow.color("yellow")
+green.color("green")
+
+# Меняем им скорость
+red.speed(0)
+blue.speed(0)
+yellow.speed(0)
+green.speed(0)
+
+# Синяя черепашка рисует
+blue.penup()
+blue.goto(-200, -150)
+blue.pendown()
+
+for i in range(60):
+    blue.color(i / 60, 0, 1 - i / 360)
+    blue.forward(i * 2)
+    blue.right(59)
+    for _ in range(2):
+        blue.forward(i* 2)
+        blue.right(59)
+
+# зелёная черепашка рисует квадрат
+green.penup()
+green.goto(-200, 150)
+green.pendown()
+
+for _ in range(4):
+    green.forward(100)
+    green.right(90)
+
+# Желтая черепашка рисует солнце
+yellow.penup()
+yellow.goto(200, 150)
+yellow.pendown()
+yellow.pensize(4)
+
+yellow.begin_fill()
+for _ in range(22):
+    yellow.right(45)
+    yellow.forward(50)
+    yellow.left(145)
+    yellow.forward(50)
+yellow.end_fill()
 
 
-# Рисуем солнце
-pen.color('yellow')
-
-pen.begin_fill()
-for i in range(72):
-    pen.left(5)
-    pen.forward(5)
-
-pen.end_fill()
 
 
-# Делаем так чтобы часть солнца было закрыто морем
-pen.left(90)
-pen.forward(30)
-pen.right(90)
+# Красная черепашка рисует круговую спираль
+red.penup()
+red.goto(200, -150)
+red.pendown()
 
-# Рисуем прямоугольник (море)
-pen.color("blue")
-pen.begin_fill()
-pen.forward(300)
-pen.right(90)
-pen.forward(200)
-pen.right(90)
-pen.forward(600)
-pen.right(90)
-pen.forward(200)
-pen.right(90)
-pen.forward(300)
-pen.end_fill()
+for _ in range(36):
+    for _ in range(36):
+        red.forward(10)
+        red.right(10)
+    red.right(10)
 
 
 
-turtle.mainloop()
+
+screen.mainloop()
