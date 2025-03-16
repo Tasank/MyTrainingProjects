75f25544c8388eeeeb2605204af1404cc3e3f13c
Tasank <apanasovich04@mail.ru>
2025-03-17 01:59:59 +0500

    добавление 17.03.2025

---
+++ Обучение/Задачки/Python/Популярные библиотеки (модули)/tkinter/task_1.py
@@ -0,0 +1,28 @@
+from tkinter import *
+
+# Создание скелета интерфейса
+root = Tk()
+
+# Настройка интерфейса
+root.title('Мой первый графический интерфейс')
+root.geometry('400x300')
+root['bg'] = 'black'
+
+# Создание виджета
+text = Label(root, text='Первый виджет', font='Arial 16', bg='black', fg='white')
+# Команда pack() размещает виджет в интерфейсе
+text.pack()
+
+text_2 = Label(root, text='𓁈𓂀𓋹𓆣𓁀𓀾', font='Arial 100', bg='black', fg='white')
+text_2.pack()
+
+# Создание функциональности
+def delete_text():
+    text.destroy()
+
+# Создание кнопки
+button = Button(root, text='Узнать тайну', font='Arial 16', bg='black', fg='white',
+    activebackground='white', activeforeground='black', command=delete_text)
+button.pack()
+
+root.mainloop()
