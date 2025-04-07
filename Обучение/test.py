01a0f0caa2820a5961ba57d4f5733ae117a5ac65
Tasank <apanasovich04@mail.ru>
2025-04-07 20:52:53 +0500

    обновление 07.04.2025

--- Обучение/test.py
+++ Обучение/test.py
@@ -1,9 +1,27 @@
-my_dict = {
-    10: 'a',
-    20: 'b',
-    30: 'c',
-    'jadkjas': 1213123
-}
+from tkinter import *
+import time
 
-result = list(my_dict.keys())
-print(result[0])
+
+root = Tk()
+root.geometry("800x600")
+root.title("Анимация")
+root["bg"] = "white"
+
+
+canvas = Canvas(root, width=800,height=600,bg="white")
+
+
+def draw():
+    canvas.move(hero,1,2)
+
+
+hero = canvas.create_oval(80,80,120,120,fill="red")
+
+
+
+while True:
+    root.update()
+    root.update_idletasks()
+    draw()
+    time.sleep(0.01)
+
