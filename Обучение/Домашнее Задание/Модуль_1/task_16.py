438c58c478ad46966cf149dad6f7134d3174af1a
Tasank <apanasovich04@mail.ru>
2025-04-07 22:54:47 +0500

    релиз 07.04.2025

--- Обучение/Домашнее Задание/Модуль_1/task_16.py
+++ Обучение/Домашнее Задание/Модуль_1/task_16.py
@@ -8,20 +8,28 @@ root.geometry("800x600")
 root.title("Анимация")
 root["bg"] = "white"
 
-canvas = Canvas(root, width=800, height=600, bg="white")
-canvas.pack()
+canvas = Canvas(root, width=700, height=600, bg="white")
+canvas.grid(row=0, column=0, rowspan=7)
 
+# Создаём дуло
 canvas.create_rectangle(0, 180, 40, 200, fill="black")
 canvas.create_rectangle(0, 250, 40, 270, fill="black")
+
 
 bullets = []
 bullet_color = "red"
 
 def shot():
-    hero = canvas.create_oval(5, 205, 45, 245, fill=bullet_color)
-    b = (hero, 10, random.random())
-    bullets.append(b)
-    root.after(50, shot)
+    if len(bullets) < 5:
+        hero = canvas.create_oval(5, 205, 45, 245, fill=bullet_color)
+        b = (hero, 10, random.random())
+        bullets.append(b)
+        root.after(50, shot)  # Задержка между выстрелами в очереди
+
+
+# Создаём красивую жёлтую кнопку огонь
+fire = Button(root, text="Огонь👉", font='Arial 16', bg='yellow', fg='black', command=shot)
+fire.grid(row=0, column=1)
 
 # Изменяем цвет пули на нажатие клавиши V
 def ask_color(event):
@@ -44,15 +52,8 @@ def draw():
             canvas.delete(bullet)
             bullets.remove(bull)
 
-
-
-
-
-shot()
 while True:
    root.update()
    root.update_idletasks()
    draw()
    time.sleep(0.01)
-
-
