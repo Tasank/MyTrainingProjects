5d5b97a893a1bdbd9addc05003b80f16723ab773
Tasank <apanasovich04@mail.ru>
2025-04-07 11:03:19 +0500

    обновление 16.04.2025

--- Обучение/Домашнее Задание/Модуль_1/task_16.py
+++ Обучение/Домашнее Задание/Модуль_1/task_16.py
@@ -1,48 +1,47 @@
-"""
-Делаем анимацию пулемёта с помощью tkinter
-"""
-
+import random
 import time
 from tkinter import *
 
 root = Tk()
 root.geometry("800x600")
 root.title("Анимация")
-root["bg"] = 'white'
+root["bg"] = "white"
 
 canvas = Canvas(root, width=800, height=600, bg="white")
 canvas.pack()
 
-# Создаём 2 объекта которые нарисуют простенькое дуло
 canvas.create_rectangle(0, 180, 40, 200, fill="black")
 canvas.create_rectangle(0, 250, 40, 270, fill="black")
 
-# Создаём пулю (шарик красный)
-hero = canvas.create_oval(5, 205, 45, 245, fill="red")
+bullets = []
 
-# Переменные скорости
-h_x = 5
-h_y = 0
+def shot():
+    hero = canvas.create_oval(5, 205, 45, 245, fill="red")
+    b = (hero, 10, random.random())
+    bullets.append(b)
+    root.after(50, shot)
 
 def draw():
-    global h_x, h_y, e_x, e_y
-    x, y, x1, y1 = canvas.coords(hero)
 
-    # Проверки для красного шарика
-    if x <= 0:  # если мяч коснулся левой стенки
-        h_x = -h_x  # меняем его Х-скорость на противоположную
-    if x1 >= 800:  # если мяч коснулся правой стенки
-        h_x = -h_x  # меняем его Х-скорость на противоположную
+    for bull in bullets:
+        bullet = bull[0]
+        h_x = bull[1]
+        h_y = bull[2]
+        canvas.move(bullet, h_x, h_y)
+        x, y, x1, y1 = canvas.coords(bullet)
+        if x1 >= 800:
+            canvas.delete(bullet)
+            bullets.remove(bull)
 
-    if y <= 0:
-        h_y = -h_y
-    if y1 >= 600:
-        h_y = -h_y
 
-    canvas.move(hero, h_x, h_y)
 
+
+
+shot()
 while True:
-    root.update()
-    root.update_idletasks()
-    draw() # Зацикливаем движение
-    time.sleep(0.01)
+   root.update()
+   root.update_idletasks()
+   draw()
+   time.sleep(0.01)
+
+
