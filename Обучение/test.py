cb4c1b4914f1645b09883e3aa7b8a8262510cdc1
Tasank <apanasovich04@mail.ru>
2025-04-16 20:57:15 +0500

    обновление 16.04.2025

---
+++ Обучение/test.py
@@ -0,0 +1,9 @@
+animal = {'пантера': 'хищник',
+          'белка': 'травоядная',
+          'окунь': 'рыба',
+          'атмосфера': 'планета'}
+
+animal_keys = list(animal.keys())
+
+print(animal_keys)
+print(type(animal_keys))
