7d4b01e52a715fa332e3fde2ce576381ad02d1ee
Tasank <apanasovich04@mail.ru>
2025-04-18 21:36:26 +0500

    обновление 18.04.2025

--- Обучение/test.py
+++ Обучение/test.py
@@ -1,3 +1,8 @@
-import time
 
-time.time_ns() # возвращает количество наносекунд, прошедших с начала эпохи Unix
+max_name = 'тут'
+ez_name = 'Жираф'
+
+if len(max_name) > len(ez_name):
+    max_name = ez_name
+
+print(max_name)
