3e6fd6c1b13028e10c8d1b3013db6e916aed2727
Tasank <apanasovich04@mail.ru>
2025-04-26 19:34:57 +0500

    update 26.04.2025

--- Обучение/test.py
+++ Обучение/test.py
@@ -1,8 +1,5 @@
+def plus(a, b):
+    print(a + b)
 
-max_name = 'тут'
-ez_name = 'Жираф'
-
-if len(max_name) > len(ez_name):
-    max_name = ez_name
-
-print(max_name)
+result = plus(5, 6)
+print(result)
