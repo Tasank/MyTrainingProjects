c80fa4524bfcebae67192838f6df395689231976
Tasank <apanasovich04@mail.ru>
2025-05-03 12:02:36 +0500

    up 03.05.2025

--- Обучение/test.py
+++ Обучение/test.py
@@ -1,14 +1,11 @@
-from tkinter import *
-root=Tk()
-root.geometry("600x400")
-root.title("окошко")
-root["bg"]= "green"
-text=Label(root, text='𓁈𓂀𓋹𓆣𓁀𓀾',font=("Consolas",100 , "bold"),fg="black",bg="dark green" )
-text.pack()
-x=Label(root, text="Бог Ра-бог солнца", bg="dark green" )
-x.pack()
+import turtle
+t = turtle. Pen()
+t.color("yellow")
+t.begin_fill()
 
-btn = Button(root, text="Нажми на меня!", fg="red", font=("Algerian",23),  bg="dark green")
-btn.pack()
+for x in range(80):
+    t.forward(5)
+    t.left(5)
+t.end_fill()
 
-root.mainloop()
+turtle.mainloop()
