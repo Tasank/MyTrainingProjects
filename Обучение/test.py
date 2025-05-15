dddb28666abaf404f3a039a182a3ccd712ee25c4
Tasank <apanasovich04@mail.ru>
2025-05-15 23:09:14 +0500

    up 15.05.2025

--- Обучение/test.py
+++ Обучение/test.py
@@ -1,5 +1,12 @@
 
-def hello():
-    print('Привет')
-    hello()
-hello()
+
+class Telephone:
+    def __init__(self):
+        self.mark = None
+        self.model = None
+        self.price = None
+        self.width = None
+        self.height = None
+
+    def photo(self):
+        pass
